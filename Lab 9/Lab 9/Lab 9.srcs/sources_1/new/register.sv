a3e6061321dac6b7577bf4bcbb0abda1bb50bfd3
Chris041400 <59978912+Chris041400@users.noreply.github.com>
2020-04-01 18:26:43 -0500

    Change

---
+++ Lab 9/Lab 9/Lab 9.srcs/sources_1/new/register.sv
@@ -0,0 +1,23 @@
+`module register #(parameter N=1) 
+    (
+    input clk, rst, en, 
+    input [N-1:0] D, 
+    output reg [N-1:0] Q
+    );
+    
+    always @(posedge clk, posedge rst
+    begin
+        if (rst ==1)
+            Q <= /*???*/ ; 
+        else if (en==1) 
+            Q <= /*???*/ ;
+end
+// Notes:  
+// - Reset is asynchronous , so this 
+// block needs to execute when rst 
+// goes high. 
+// - We want enable to be synchronous 
+// (i.e. only happens on rising 
+// edge of clk), so it is left out 
+// of "sensitivity" list.
+endmodule
